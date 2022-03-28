0b4d6f9db6992891ef404804742046855ecdbe4a
ChristianEnfors <Christian@wishfully.se>
2022-03-28 08:41:28 +0200

    more work

--- StealthGame/Stealth Game/Assets/Scripts/Guard.cs
+++ StealthGame/Stealth Game/Assets/Scripts/Guard.cs
@@ -9,13 +9,46 @@ public class Guard : MonoBehaviour
     public float distanceToWaypoint = 1f;
     public float waitTime = 1;
     public float turnSpeed = 90;
+    public Light spotLight;
+
+    Transform player;
+
 
 
     private void Start()
     {
+
         StartCoroutine(Patrol());
     }
+
+    bool CanSeePlayer()
+    {
+        if (Vector3.Distance(transform.position, player.position) < 10f)
+        {
+            // vinklen mellan vector3.forward och spelaren delat på 2.
+
+            Vector3 directionToPlayer = (transform.forward - player.position).normalized;
+            float angleToPlayer = Mathf.Atan2(directionToPlayer.y, directionToPlayer.x) * Mathf.Rad2Deg;
+
+            if (angleToPlayer < spotLight.spotAngle / 2)
+            {                
+                return true;                
+            }
+        }
+
+        return false;
+    }
     
+
+
+    private void Update()
+    {
+
+        
+    }
+
+
+
 
     IEnumerator Patrol()
     {
@@ -24,36 +57,40 @@ public class Guard : MonoBehaviour
 
             foreach (Transform waypoint in pathHolder)
             {
-                yield return StartCoroutine(Move(waypoint.position, speed));
-                yield return new WaitForSeconds (waitTime);
                 yield return StartCoroutine(TurnToFace(waypoint.position));
+                yield return StartCoroutine(Move(waypoint.position, speed));
+                yield return new WaitForSeconds(waitTime);
+
             }
         }
     }
-        
+
     IEnumerator Move(Vector3 destination, float speed)
     {
         while (transform.position != destination)
-        {            
+        {
             transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
-            yield return null;             
+            yield return null;
         }
     }
 
     IEnumerator TurnToFace(Vector3 destination)
     {
         Vector3 dirToLookTarget = (destination - transform.position).normalized;
+
         float targetAngle = 90 - Mathf.Atan2(dirToLookTarget.z, dirToLookTarget.x) * Mathf.Rad2Deg;
-       
-        while(Mathf.DeltaAngle(transform.eulerAngles.y, targetAngle) > 0.05f)
+
+
+        while (Mathf.DeltaAngle(transform.eulerAngles.y, targetAngle) > 0.05f)
         {
             float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetAngle, turnSpeed * Time.deltaTime);
             transform.eulerAngles = Vector3.up * angle;
             yield return null;
-            
+
         }
     }
-        
+
+
 
 
 

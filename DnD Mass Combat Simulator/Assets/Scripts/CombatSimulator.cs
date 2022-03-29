132b0b5ba48192a9f2b14de7248c28a22766aed6
ChristianEnfors <Christian@wishfully.se>
2022-03-29 17:35:37 +0200

    ComatSim version 1.0 done and build!

--- DnD Mass Combat Simulator/Assets/Scripts/CombatSimulator.cs
+++ DnD Mass Combat Simulator/Assets/Scripts/CombatSimulator.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class CombatSimulator : MonoBehaviour
 {
@@ -10,14 +11,33 @@ public class CombatSimulator : MonoBehaviour
     public int attackDamageDie;
     public int attackDamageDieCount;
     public int attackersToHit;
-    public int averageDamage;
 
     public TextMeshProUGUI attackersOutput;
     public TextMeshProUGUI damageOutput;
     public TextMeshProUGUI averageDamageOutput;
-    
+    public TextMeshProUGUI noOfDieOutput;
+
+    public GameObject inputParent;
+
+    TMP_InputField attackersInput;
+    TMP_InputField playerACInput;
+    TMP_InputField attackersToHitInput;
+    TMP_Dropdown attackDieInput;
+    Slider noOfDiesInput;
+
+
+    int averageDamage;
     int howManyHit;
     int totalDMG;
+
+    private void Start()
+    {
+        attackersInput = inputParent.transform.GetChild(1).GetComponent<TMP_InputField>();
+        playerACInput = inputParent.transform.GetChild(3).GetComponent<TMP_InputField>();
+        attackDieInput = inputParent.transform.GetChild(5).GetComponent<TMP_Dropdown>();
+        noOfDiesInput = inputParent.transform.GetChild(7).GetComponent<Slider>();
+        attackersToHitInput = inputParent.transform.GetChild(10).GetComponent<TMP_InputField>();
+    }
 
     private void Update()
     {
@@ -25,33 +45,48 @@ public class CombatSimulator : MonoBehaviour
         {
             MassCombatSimulator();
         }
-    }
 
-    public void MassCombatSimulator()
+        int.TryParse(attackersInput.text, out attackers);
+        int.TryParse(playerACInput.text, out playerAC);
+        int.TryParse(attackersToHitInput.text, out attackersToHit);
+
+        string attackDamageDieString = attackDieInput.options[attackDieInput.value].text;
+        attackDamageDie = int.Parse (attackDamageDieString.TrimStart('D'));
+
+        attackDamageDieCount = (int)noOfDiesInput.value;
+        noOfDieOutput.text = attackDamageDieCount.ToString();
+
+
+
+
+
+}
+
+public void MassCombatSimulator()
+{
+    howManyHit = 0;
+    totalDMG = 0;
+    averageDamage = 0;
+
+    for (int i = 0; i < attackers; i++)
     {
-        howManyHit = 0;
-        totalDMG = 0;
-        averageDamage = 0;
+        int dieRoll = Random.Range(1, 20);
 
-        for (int i = 0; i < attackers; i++)
+        if (dieRoll + attackersToHit >= playerAC)
         {
-            int dieRoll = Random.Range(1, 20);
+            howManyHit++;
 
-            if (dieRoll + attackersToHit >= playerAC)
-            {
-                howManyHit++;
-
-                totalDMG = totalDMG + (Random.Range(1, attackDamageDie) * attackDamageDieCount);
-                averageDamage = totalDMG / howManyHit;
-            }
-
+            totalDMG = totalDMG + (Random.Range(1, attackDamageDie) * attackDamageDieCount);
+            averageDamage = totalDMG / howManyHit;
         }
 
-        attackersOutput.text = howManyHit.ToString();
-        averageDamageOutput.text = averageDamage.ToString();
-        damageOutput.text = totalDMG.ToString() + "!";
-
     }
+
+    attackersOutput.text = howManyHit.ToString();
+    averageDamageOutput.text = averageDamage.ToString();
+    damageOutput.text = totalDMG.ToString() + "!";
+
+}
 
 
 }
